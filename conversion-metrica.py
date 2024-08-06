c6728dfe479c2fb63b72becda90b4621f8079acc
Sofia <acostasofi029@gmail.com>
2024-08-06 18:36:24 -0300

    Realizamos una solucion en python para la practica final 2 donde hicimos un analisis completo de una problematica de una empresa ficticia.

---
+++ conversion-metrica.py
@@ -0,0 +1,6 @@
+
+medida_en_cms = float(input("Por favor, ingresar las medidas de la pieza de mueble en cms:  "))
+
+medidas_en_pulgadas = medida_en_cms / 2.54
+
+print("Las medidas en pulgadas de la pieza ingresada son:  ", medidas_en_pulgadas)
